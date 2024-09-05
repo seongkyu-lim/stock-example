e395df0db7c601746dadcad86e2c68ecaf79fbb7
seongkyulim <96x60812@gmail.com>
2024-09-05 20:58:09 +0900

    feat: optimistic lock

--- src/main/java/com/example/stock/service/OptimisticLockStockService.java
+++ src/main/java/com/example/stock/service/OptimisticLockStockService.java
@@ -1,24 +1,24 @@
-// package com.example.stock.service;
+package com.example.stock.service;
 
-// import com.example.stock.domain.Stock;
-// import com.example.stock.repository.StockRepository;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// @Service
-// public class OptimisticLockStockService {
+@Service
+public class OptimisticLockStockService {
 
-// private StockRepository stockRepository;
+private StockRepository stockRepository;
 
-// public OptimisticLockStockService(StockRepository stockRepository) {
-// this.stockRepository = stockRepository;
-// }
+public OptimisticLockStockService(StockRepository stockRepository) {
+this.stockRepository = stockRepository;
+}
 
-// @Transactional
-// public void decrease(Long id, Long quantity) {
-// Stock stock = stockRepository.findByIdWithOptimisticLock(id);
-// stock.decrease(quantity);
+@Transactional
+public void decrease(Long id, Long quantity) {
+Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+stock.decrease(quantity);
 
-// stockRepository.saveAndFlush(stock);
-// }
-// }
+stockRepository.saveAndFlush(stock);
+}
+}
