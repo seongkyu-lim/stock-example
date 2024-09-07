9e5f5e1bc6687263fa2f8d9f5f7fc4a6ecc78436
seongkyulim <96x60812@gmail.com>
2024-09-07 16:14:45 +0900

    feat: lettuce

--- src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -1,31 +1,34 @@
-// package com.example.stock.facade;
+package com.example.stock.facade;
 
-// import com.example.stock.repository.RedisLockRepository;
-// import com.example.stock.service.StockService;
-// import org.springframework.stereotype.Component;
+import com.example.stock.repository.RedisLockRepository;
+import com.example.stock.service.StockService;
+import org.springframework.stereotype.Component;
 
-// @Component
-// public class LettuceLockStockFacade {
+@Component
+public class LettuceLockStockFacade {
 
-// private RedisLockRepository redisLockRepository;
+private RedisLockRepository redisLockRepository;
 
-// private StockService stockService;
+private StockService stockService;
 
-// public LettuceLockStockFacade(RedisLockRepository redisLockRepository,
-// StockService stockService) {
-// this.redisLockRepository = redisLockRepository;
-// this.stockService = stockService;
-// }
+public LettuceLockStockFacade(RedisLockRepository redisLockRepository,
+StockService stockService) {
+this.redisLockRepository = redisLockRepository;
+this.stockService = stockService;
+}
 
-// public void decrease(Long key, Long quantity) throws InterruptedException {
-// while (!redisLockRepository.lock(key)) {
-// Thread.sleep(100);
-// }
-
-// try {
-// stockService.decrease(key, quantity);
-// } finally {
-// redisLockRepository.unlock(key);
-// }
-// }
-// }
+public void decrease(Long key, Long quantity) throws InterruptedException {
+    //락을 획득할 때까지 대기 하다가
+while (!redisLockRepository.lock(key)) {
+Thread.sleep(100);
+}
+할
+try {
+stockService.decrease(key, quantity);
+} finally {
+    // 로직 종료되 락 해제.
+redisLockRepository.unlock(key);
+}
+}
+}
+면
